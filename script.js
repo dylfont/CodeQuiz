44e65cc649966206c80a1419bcc09020371b92d7
Dylan <dylanfontenot@protonmail.com>
2021-09-07 19:35:23 -0500

    starter files and first edits to html and js

---
+++ script.js
@@ -0,0 +1,26 @@
+var startSection = document.getElementById("startSection")
+var startBTN = document.getElementById("startBTN")
+var timer = document.getElementById("timer")
+var time = 60
+
+
+
+
+
+function startGame(){
+    var gameTimer = setInterval(function(){
+
+    },1000)
+}
+
+
+
+
+
+
+
+
+
+
+
+startBTN.addEventListener("click",startGame)
